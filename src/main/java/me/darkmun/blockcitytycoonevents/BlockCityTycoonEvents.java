88af0bd6cf36dae48be44db723d0d45aa763f6bc
Евгений Матюнин <matyunin.ey@students.dvfu.ru>
2022-09-30 21:51:30 +1000

    Создал команду-тест для проверки изменения чужого конфига

--- src/main/java/me/darkmun/blockcitytycoonevents/BlockCityTycoonEvents.java
+++ src/main/java/me/darkmun/blockcitytycoonevents/BlockCityTycoonEvents.java
@@ -1,12 +1,18 @@
 package me.darkmun.blockcitytycoonevents;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BlockCityTycoonEvents extends JavaPlugin {
-
+public final class BlockCityTycoonEvents extends JavaPlugin implements CommandExecutor {
+    boolean zeroIncome = false;
+    int currentIncome;
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
 
     }
 
@@ -14,4 +20,32 @@ public final class BlockCityTycoonEvents extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player pl = (Player) sender;
+            Config WFXBusinessConfig = new Config();
+            WFXBusinessConfig.setup(Bukkit.getServer().getPluginManager().getPlugin("WFX-Business").getDataFolder(), "config");
+
+
+            if (!zeroIncome) {
+                currentIncome = WFXBusinessConfig.getConfig().getInt("DataBaseIncome." + pl.getName());
+                WFXBusinessConfig.getConfig().set("DataBaseIncome." + pl.getName(), 0);
+                zeroIncome = true;
+            }
+            else {
+                WFXBusinessConfig.getConfig().set("DataBaseIncome." + pl.getName(), currentIncome);
+                zeroIncome = false;
+            }
+            WFXBusinessConfig.saveConfig();
+
+
+
+            return true;
+        }
+        return false;
+    }
+
 }
