4f0844fa2629a981218d5c0d47300550cdeabc4e
Евгений Матюнин <matyunin.ey@students.dvfu.ru>
2022-09-30 17:28:26 +1000

    Проект создан

---
+++ src/main/java/me/darkmun/blockcitytycoonevents/BlockCityTycoonEvents.java
@@ -0,0 +1,17 @@
+package me.darkmun.blockcitytycoonevents;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockCityTycoonEvents extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
